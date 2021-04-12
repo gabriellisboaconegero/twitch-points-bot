4b8b3089cd07ae6020d32e8311288a58c6183a51
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2021-04-11 23:45:11 -0300

    melhorando storage
    transformando os dados dos canais em uma lista

--- src/popup/channelsTemplate.js
+++ src/popup/channelsTemplate.js
@@ -2,24 +2,20 @@ function popupHTML(channels){
     var template = '';
 
     // para cada canal e vai pegar e criar uma string template de como deve ser o html
-    for (const channelName in channels) {
-
-        let channelData = channels[channelName];
-        console.log(channelData);
-        
-        if (channelData.online){
+    for (const channel of channels) {      
+        if (channel.online){
             // essa image pode ser tanto img ou svg, por isso as configurações são setadas antes
-            let image = channelData.image;
-            let isChannelFarming = channelData.farming
-            let channelPoints = channelData.points;
-            let channelPointsName = channelData.pointsName;
+            let image = channel.image;
+            let isChannelFarming = channel.farming
+            let channelPoints = channel.points;
+            let channelPointsName = channel.pointsName;
             let templateString = `
                 <li>
-                    <div class="ch ${channelName}">
+                    <div class="ch ${channel.name}">
 
                         <div class="ch-name ch-flex">
                             <img src="../icons/${isChannelFarming}.png" alt="${isChannelFarming? 'checked': 'unchecked'}">
-                            <span>${channelName}</span>
+                            <span>${channel.name}</span>
                         </div>
 
                         <div class="ch-pts ch-flex" title="${channelPointsName}">
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -7,15 +7,15 @@ chrome.storage.sync.get(["channelsData", "watchingNow"], function(data){
     //canal sendo visto agr
     const watching = data.watchingNow;
 
-    if (!channels.length){
+    if (channels.length !== 0){
         let ul = document.createElement("ul");
-        ul.innerHTML = popupHTML(channels)
+        ul.innerHTML = popupHTML(channels);
         document.getElementsByTagName("main")[0].appendChild(ul);
         // adiciona o evento de click na div para poder mudar o watchingNow do storage
-        for (const channelName in channels) {
-            document.getElementsByClassName(channelName)[0].addEventListener("click", function(element){
-                if (channelName !== watching){
-                    chrome.runtime.sendMessage({"msg": "select_and_move_to_tab", channelName, wasWatching:watching});
+        for (const channel of channels) {
+            document.getElementsByClassName(channel.name)[0].addEventListener("click", function(element){
+                if (channel !== watching){
+                    chrome.runtime.sendMessage({"msg": "select_and_move_to_tab", channel: channel.name, wasWatching:watching});
                 }
             });
         }
